12a085dbbb8f631ac19c0557f67c4ecb46955bf1
Sapioce <bertrand.bacon.36@gmail.com>
2024-03-05 20:26:48 +0100

    création des component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { OffersComponent } from './component/offers/offers.component';
+import { ProfilComponent } from './component/profil/profil.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OffersComponent,
+    ProfilComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
