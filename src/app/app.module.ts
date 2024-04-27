b5217e787cf63fc08071bb866f6286f697fdd479
Sapioce <bertrand.bacon.36@gmail.com>
2024-04-27 18:21:06 +0200

    creation du componenent Panier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { TokenResfreshInterceptor } from './interceptor/tokenRefresh/token-resfr
 import { AddressBlocComponent } from './component/profil/address-bloc/address-bloc.component';
 import { BankBlocComponent } from './component/profil/bank-bloc/bank-bloc.component';
 import { CGUComponent } from './component/cgu/cgu.component';
+import { PanierComponent } from './component/panier/panier/panier.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { CGUComponent } from './component/cgu/cgu.component';
     AdminComponent,
     AddressBlocComponent,
     BankBlocComponent,
-    CGUComponent
+    CGUComponent,
+    PanierComponent
   ],
   imports: [
     BrowserModule,
