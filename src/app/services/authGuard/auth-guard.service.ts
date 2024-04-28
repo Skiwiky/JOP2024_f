5bf5ab9bb1b5aff6704ea429fb8ab59534dedd4e
Sapioce <bertrand.bacon.36@gmail.com>
2024-04-28 15:29:52 +0200

    MAJ authGuard

--- src/app/services/authGuard/auth-guard.service.ts
+++ src/app/services/authGuard/auth-guard.service.ts
@@ -1,35 +1,45 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { LoginService } from '../login/login.service';
+import { StorageService } from '../storage/storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
+  constructor(private loginService: LoginService, private storageService: StorageService, private router: Router) { }
 
-  constructor(private loginService: LoginService, private router: Router) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.checkLogin(route, state.url);
+  }
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-    /*if (!this.loginService.isLoggedIn()) {
-      this.router.navigate(['/connexion']);
-      return false;
-    }*/
-
-    if (this.loginService.checkSession()) {
+  private checkLogin(route: ActivatedRouteSnapshot, url: string): boolean {
+    
+    if (this.loginService.isLoggedIn()) {
+      const userJson = this.storageService.getItemWithExpiry('user');
+      if (!userJson) {
+        this.router.navigate(['/connexion']);
+        return false;
+      }
+  
+      const user = JSON.parse(userJson);
+      const userRole = user ? user.role : null;
+      console.log('UserRole inside checkLogin:', userRole);
+      const roles = route.data['roles'] as Array<string>;
+      console.log('Required roles:', roles);
+      console.log("roles:"+ userRole )
+      if (roles && roles.indexOf(userRole) === -1) {
+        
+        this.router.navigate(['/home']);
+        return false;
+      }
       return true;
     } else {
-      this.router.navigate(['/connexion']); 
+      this.router.navigate(['/connexion'], { queryParams: { returnUrl: url } });
       return false;
     }
-
-    const userRole = this.loginService.getUserRole();
-    const roles = route.data['roles'] as Array<string>;
-
-    // controle des roles avec les roles des routes
-    if (roles && roles.indexOf(userRole) === -1) {
-      this.router.navigate(['/home']);
-      return false;
-    }
-    return true;
   }
 }
